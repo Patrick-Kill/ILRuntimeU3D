f60b1e0e63df0783c39e79fec029fd33d289ceee
Patrick <Patrick-Kill@users.noreply.github.com>
2019-12-19 16:15:18 +0800

    测试

---
+++ HotFix_Project/HotFixInit.cs
@@ -0,0 +1,19 @@
+﻿
+namespace HotFix_Project
+{
+    public class HotFixInit
+    {
+        //public HotFixInit()
+        //{
+        //    UnityEngine.Debug.Log("热更初始化");
+        //}
+
+
+        // static method
+        public static void Initialize()
+        {
+            UnityEngine.Debug.Log("热更初始化--------");
+        }
+
+    }
+}
